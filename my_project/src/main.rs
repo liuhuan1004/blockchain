f43ce12444b9ed800a309e111cefdf8166f0469c
liuhuan1004 <liuhuan1004@163.com>
2026-02-26 23:16:44 +0800

    Day 4: sum

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let nums = [1, 2, 3, 4, 5];
+    let mut sum = 0;
+
+    for n in nums {
+        sum += n;
+    }
+
+    println!("Day 4: sum = {}", sum);
 }
