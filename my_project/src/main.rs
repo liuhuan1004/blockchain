34331f5e2a0e68e74ba3a0cbe6c693086c284455
liuhuan1004 <liuhuan1004@163.com>
2026-02-28 23:07:49 +0800

    Day 6: Sum odd or even

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,14 +1,18 @@
-fn main() {
-    let nums = [1, 2, 3, 4, 5];
+fn calc_sum(nums: [i32; 5]) -> i32 {
     let mut sum = 0;
-
     for n in nums {
         sum += n;
     }
+    sum
+}
 
-    if sum > 10 {
-        println!("Day 5: sum is big: {}", sum);
+fn main() {
+    let nums = [1, 2, 3, 4, 5];
+    let result = calc_sum(nums);
+
+    if result % 2 == 0 {
+        println!("Day 6: sum {} is even", result);
     } else {
-        println!("Day 5: sum is small: {}", sum);
+        println!("Day 6: sum {} is odd", result);
     }
 }
